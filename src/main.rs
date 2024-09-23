4eaa005ae040d579b0a9a789ec3d78b3eac34616
zabackary <137591653+zabackary@users.noreply.github.com>
2024-09-23 19:58:26 +0900

    feat: add non-working implementation of the CLI

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 #![windows_subsystem = "windows"]
 
+use std::{
+    fmt::{Debug, Display},
+    path::PathBuf,
+};
+
+use clap::Parser;
 use frontend::{DaktronicsSingularUiApp, Screen};
 use iced::{theme::Palette, window::icon, Color, Font, Size};
 
@@ -7,12 +13,80 @@ mod backend;
 mod frontend;
 mod mock;
 
+/// Links the output of an Daktronics AllSport 5000 to Singular.Live
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Whether to hide the UI (run without a window). Must be used with
+    /// --start.
+    #[arg(short = 'l', long, default_value_t = false)]
+    headless: bool,
+
+    /// The configuration file. If not provided, the UI will prompt for one.
+    #[arg(short, long)]
+    config_file: Option<PathBuf>,
+
+    /// Whether to start the stream immediately. Must be used with --config-file
+    /// and --serial-path.
+    #[arg(short, long, default_value_t = false)]
+    start: bool,
+
+    /// What serial path (e.g. /dev/xxx or COM1 on Windows) to use. If not
+    /// provided, the UI will prompt for one.
+    #[arg(short = 'p', long)]
+    serial_path: Option<String>,
+}
+
+enum DSUError {
+    Iced(iced::Error),
+    HeadlessWithoutStart,
+    StartWithoutConfigSerial,
+}
+
+impl Display for DSUError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Iced(iced_error) => write!(f, "error when running ui: {}", iced_error),
+            Self::HeadlessWithoutStart => write!(f, "cannot run headless without using --start"),
+            Self::StartWithoutConfigSerial => write!(
+                f,
+                "cannot start automatically without a config file and serial path"
+            ),
+        }
+    }
+}
+
+// Hacky implementation that prints errors nicely to the CLI
+impl Debug for DSUError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        Display::fmt(&self, f)
+    }
+}
+
+impl std::error::Error for DSUError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Self::Iced(iced_error) => Some(iced_error),
+            _ => None,
+        }
+    }
+}
+
 const APP_NAME: &str = "Daktronics Singular UI";
 const APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
 const DAKTRONICS_SINGULAR_UI_PROFILE_FILE_EXTENSION: &str = "dsu";
 const GITHUB_URL: &str = "https://github.com/zabackary/daktronics-singular-ui";
 
-fn main() -> iced::Result {
+fn main() -> Result<(), DSUError> {
+    let args = Args::parse();
+
+    if args.headless && !args.start {
+        return Err(DSUError::HeadlessWithoutStart);
+    }
+    if args.start && (args.serial_path.is_none() || args.config_file.is_none()) {
+        return Err(DSUError::StartWithoutConfigSerial);
+    }
+
     let program_icon = image::load_from_memory_with_format(
         include_bytes!("../assets/logo.png"),
         image::ImageFormat::Png,
@@ -77,4 +151,5 @@ fn main() -> iced::Result {
         )
     })
     .run()
+    .map_err(DSUError::Iced)
 }
